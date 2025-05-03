d3246cec4f654af79c07982add6ee6edb2685677
megupta06 <103089880+megupta06@users.noreply.github.com>
2025-05-03 15:01:14 -0700

    Update sum.test.js

--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -1,8 +1,8 @@
 // sum.test.js
 import { sum } from '../code-to-unit-test/sum';
 test('adds 1 + 2 to equal 3', () => {
-  expect(1 + 2).toBe(10);
+  expect(1 + 2).toBe(6);
 });
 test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1,2)).toBe(9);
+  expect(sum(1,2)).toBe(3);
 });
